0b41a44aa8e520aa44db4bdd919da069423243ca
javiro3004@gmail.com <javiro3004@gmail.com>
2025-01-12 07:57:21 -0500

    primero cambios

---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import './button.css'
+
+interface props {
+    label : string
+    parentMethod : () => void
+}
+
+export const Button = ({label,parentMethod}:props) => {
+    return (
+        <button className='custom-button' onClick={parentMethod}>
+            {label}
+        </button>
+    )
+}
